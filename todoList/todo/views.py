dfeda6f33f9547f860cd0b15afde717b7e0d8720
Douglas Mogoba <mogobanyamwaro@gmail.com>
2021-05-10 18:58:58 +0300

    the todo list using django

---
+++ todoList/todo/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from .models import Todo
+
+# Create your views here.
+
+def index(request):
+    if request.method =='POST':
+        new_todo = Todo(
+            title = request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+    return render(request,'index.html')
